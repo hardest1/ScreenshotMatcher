72bf8971838259327539643ff79787cb06b9bbaf
Alex <alex_ha@live.de>
2020-04-22 05:45:06 +0200

    Move declarations from source to header

--- main.hpp
+++ main.hpp
@@ -6,22 +6,38 @@
 #include <string>
 #include <limits.h>
 #include <thread>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
 
 #include <libappindicator/app-indicator.h>
 
-
+#include "qrcodegen/QrCode.hpp"
 #include "httplib/httplib.h"
 #include "daemonize/daemonize.hpp"
 #include "matchscreenshot/matchscreenshot.hpp"
+#include "server/server.hpp"
+#include "logger/logger.hpp"
 
 using namespace std;
 using namespace httplib;
 
-Server svr;
+using std::uint8_t;
+using qrcodegen::QrCode;
+using qrcodegen::QrSegment;
 
+string results_folder;
 string serviceURL;
 
 // Server Config
 string host;
 int port;
 bool startAsDaemon;
+
+// Tray
+GtkWidget*      indicator_menu;
+GtkActionGroup* action_group;
+GtkUIManager*   uim;
+AppIndicator* indicator;
+GError* gerror = NULL;
